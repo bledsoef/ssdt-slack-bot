cfb3808323ead546f30817f9a004f0a4bd2a9269
bledsoef <bledsoef@berea.edu>
2023-03-05 19:44:22 -0500

    fixed most formatting, still need to fix the dates

--- weeklymessage/weeklymessage.go
+++ weeklymessage/weeklymessage.go
@@ -67,20 +67,27 @@ func main() {
 }
 
 func getOutstandingPRs() string {
-	message := ""
+	message := "*Good morning everyone!*"
+	var needsReview []*github.PullRequest
+	var needsChanges []*github.PullRequest
+
+	// set up authentication and api variables
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: "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"},
 	)
+
 	tc := oauth2.NewClient(ctx, ts)
 	githubClient := github.NewClient(tc)
+
+	// fetch all of the repositories from the SSDT
 	owner := "BCStudentSoftwareDevTeam"
 	repos, _, err := githubClient.Repositories.List(ctx, owner, nil)
-
 	if err != nil {
 		fmt.Printf("Failed to list repositories: %v\n", err)
 	}
 
+	// compile all of the repos' PRs into one list
 	var allPRs []*github.PullRequest
 	for _, repo := range repos {
 		prs, _, err := githubClient.PullRequests.List(ctx, owner, repo.GetName(), &github.PullRequestListOptions{
@@ -93,7 +100,11 @@ func getOutstandingPRs() string {
 
 		allPRs = append(allPRs, prs...)
 	}
+	noReviews := false
+	lastReview := time.Now()
+	lastCommit := time.Now()
 
+	// iterate through the prs and identify if they are outstanding.
 	for _, pr := range allPRs {
 		// Get the list of reviews for the Pull Request
 		reviews, _, err := githubClient.PullRequests.ListReviews(ctx, owner, *pr.GetBase().GetRepo().Name, pr.GetNumber(), nil)
@@ -101,64 +112,66 @@ func getOutstandingPRs() string {
 			fmt.Printf("Failed to list reviews for Pull Request #%d: %v\n", pr.GetNumber(), err)
 			continue
 		}
+		// initialize boolean values to determine if a pr is outstanding or inactice
 
-		// Check if any of the reviews have been submitted within the last 48 hours
-		needsReview := true
-		needsChanges := true
-		lastReview := ""
-		sinceReview := ""
+		noReviews = false
 
-		if len(reviews) > 0 {
-			lastReview := reviews[len(reviews)-1].GetSubmittedAt()
-
-			sinceReview := time.Since(lastReview)
-			if (sinceReview < 48*time.Hour) || (730*time.Hour < sinceReview) {
-				needsReview = false
-			}
-			fmt.Println(sinceReview)
-			fmt.Println(730 * time.Hour)
-			fmt.Println(pr.GetNumber())
-		}
+		lastReview = time.Now()
 
 		commits, _, err := githubClient.PullRequests.ListCommits(ctx, owner, *pr.GetBase().GetRepo().Name, pr.GetNumber(), nil)
 		if err != nil {
 			fmt.Printf("Failed to list commits for Pull Request #%d: %v\n", pr.GetNumber(), err)
 			continue
 		}
+		lastCommit := commits[len(commits)-1].Commit.Committer.Date
 
-		lastCommit := ""
-		sinceCommit := ""
+		// if a review isn't currently in then set the last review time to be the same as the last
+		if len(reviews) > 0 {
+			lastReview = reviews[len(reviews)-1].GetSubmittedAt()
+		} else {
+			lastReview = *lastCommit
+			noReviews = true
+		}
+		sinceReview := time.Since(lastReview)
+		sinceCommit := time.Since(*lastCommit)
 
-		if len(commits) > 0 {
-			lastCommit := commits[len(commits)-1].Commit.Committer.Date
-			sinceCommit := time.Since(*lastCommit)
-			if (sinceCommit < 48*time.Hour) || (730*time.Hour < sinceCommit) {
-				needsChanges = false
-			}
-			fmt.Println(sinceReview)
-			fmt.Println(730 * time.Hour)
-			fmt.Println(pr.GetNumber())
+		// check to see if the PR is inactive
+		if (noReviews || (sinceReview > 730*time.Hour)) || (sinceCommit > 730*time.Hour) {
+			continue
 		}
 
-		if needsChanges && needsReview {
-			if (lastReview != "") && (lastCommit != "") {
-				if sinceReview <= sinceCommit {
-					needsReview = true
-				} else if sinceReview > sinceCommit {
-					needsChanges = true
-				}
+		// if there have recently been changes and still no review then a review is required
+		if (sinceCommit > 48*time.Hour) && ((sinceReview > sinceCommit) || noReviews) {
+			needsReview = append(needsReview, pr)
 
-			} else if lastReview != "" {
-				needsReview = true
-			}
 		}
 
-		// Mark the Pull Request as needing review if no review has been submitted within the last 24 hours
+		// if there has recently been a review and still no changes then changes are required
+		if !noReviews && ((sinceReview > 48*time.Hour) && (sinceReview < sinceCommit)) {
+			needsChanges = append(needsChanges, pr)
+
+		}
+
 		// if needsReview {
 		// 	fmt.Printf("Pull Request #%d needs review\n", pr.GetNumber())
 		// } else if needsChanges {
 		// 	fmt.Printf("Pull Request #%d needs changes\n", pr.GetNumber())
 		// }
+	}
+	// loop throught the prs that require a review and add them to the message
+	message += "*The PRs that require a review are:* \n"
+	for i, review := range needsReview {
+		message += fmt.Sprintf("%s. %s: #%s %s. Last updated on %s. \n", fmt.Sprint(i+1), *review.GetBase().GetRepo().Name, fmt.Sprint(review.GetNumber()), *review.Title, lastCommit.Format("January 2, 2006"))
+	}
+
+	// loop throught the prs that require changes and add them to the message
+	message += "*The PRs that require changes are:* \n"
+	for i, changes := range needsChanges {
+		if !noReviews {
+			message += fmt.Sprintf("%s. %s: #%s %s. Last reviewed on %s. \n", fmt.Sprint(i+1), *changes.GetBase().GetRepo().Name, fmt.Sprint(changes.GetNumber()), *changes.Title, lastReview.Format("January 2, 2006"))
+		} else {
+			message += fmt.Sprintf("%s. %s: #%s %s. Submitted on %s. \n", fmt.Sprint(i+1), *changes.GetBase().GetRepo().Name, fmt.Sprint(changes.GetNumber()), *changes.Title, lastCommit.Format("January 2, 2006"))
+		}
 	}
 	return message
 
